b074721741a761adf2d2ba43de9e9af711b60ced
nanda24 <nandawahyuwijayanto@gmail.com>
2023-05-19 22:07:47 +0700

    add dio

--- flutterprojectapps/lib/listViewPage.dart
+++ flutterprojectapps/lib/listViewPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class ListViewPage extends StatefulWidget {
   const ListViewPage({super.key});
@@ -8,12 +9,31 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
+
+  final dio = Dio();
+
+  @override
+  void initState() {
+    readyMovieList();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("MOVIE LIST"),
       ),
+      body: Container(
+        child: Text("Movie Desc"),
+      ),
     );
+  }
+
+  readyMovieList() async{
+
+    final response = await dio.get('https://dlabs-test.irufano.com/api/movie?size=10&page=1');
+    print(response);
+
   }
 }

0eba48ef7dc5e16995b08b9a4b8a7fbdc442cc40
nanda24 <nandawahyuwijayanto@gmail.com>
2023-05-19 23:11:47 +0700

    add listview and json response

--- flutterprojectapps/lib/listViewPage.dart
+++ flutterprojectapps/lib/listViewPage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
@@ -10,6 +12,7 @@ class ListViewPage extends StatefulWidget {
 
 class _ListViewPageState extends State<ListViewPage> {
 
+  List movie = [];
   final dio = Dio();
 
   @override
@@ -25,15 +28,44 @@ class _ListViewPageState extends State<ListViewPage> {
         title: const Text("MOVIE LIST"),
       ),
       body: Container(
-        child: Text("Movie Desc"),
+        child: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: movie.length,
+          itemBuilder: (BuildContext context, int index) {
+
+            print(movie[index]);
+
+            return ListTile(
+              leading: FadeInImage(
+                image: NetworkImage(movie[index]['poster']), 
+                placeholder: AssetImage('assets/placeholder_image.jpg'),
+                width: 50,
+                height: 50,
+              ),
+              title: Text(movie[index]['title']),
+              subtitle: Text(
+                          movie[index]['description'],
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: true,
+                          maxLines: 3,
+                        ),
+              tileColor: Colors.green,
+              
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const Divider(),
+        ),
       ),
     );
   }
 
   readyMovieList() async{
 
-    final response = await dio.get('https://dlabs-test.irufano.com/api/movie?size=10&page=1');
-    print(response);
-
+    Response response;
+    response = await dio.get('https://dlabs-test.irufano.com/api/movie?size=10&page=1');
+    var data = response.data['data'];
+    for(int i=0;i<data.length;i++){
+      movie.add(data[i]);
+    }
   }
 }

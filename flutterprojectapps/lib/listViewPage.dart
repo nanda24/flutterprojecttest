bdc39d53efac1dfb060fc6ece4a00614a1717928
nanda24 <nandawahyuwijayanto@gmail.com>
2023-05-19 21:52:38 +0700

    add list view page

---
+++ flutterprojectapps/lib/listViewPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MOVIE LIST"),
+      ),
+    );
+  }
+}
